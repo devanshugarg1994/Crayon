95716cc55fa8b2581d80771b850dfef45e4d860f
devanshu <devanshugarg1994@gmail.com>
2020-08-08 14:47:01 +0530

    Collision

--- public/src/main.js
+++ public/src/main.js
@@ -2,10 +2,9 @@ import {createMario} from './entities.js'
 import Timer from './Timer.js'
 import KeyboardState from './KeyBoardState.js'
 import {loadLevel} from './loader.js'
+import {createCollisionLayer} from './Layers.js'
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-
-
 // Parallel Loading of Sprites using Promise all
 Promise.all([
     createMario(),
@@ -14,6 +13,8 @@ Promise.all([
 .then(([mario,  level]) => {
     const gravity = 2000;
     mario.pos.set(64, 180);
+    level.comp.layers.push(createCollisionLayer(level));
+
     level.entities.add(mario);
     // Adding Key and there response
     const input = new KeyboardState();
@@ -23,8 +24,23 @@ Promise.all([
         } else {
             mario.jump.cancel();
         }
-    })
+    });
+    input.addMapping(39, keyState => {
+        mario.go.direction = keyState;
+    });
+    input.addMapping(37, keyState => {
+        mario.go.direction = -keyState;
+    });
     input.listenTo(window);
+
+    ['mousedown' , 'mouseup'].forEach(eventName => {
+        canvas.addEventListener(eventName, evt => {
+            if(evt.buttons === 1){
+                mario.vel.set(0, 0);
+                mario.pos.set(evt.offsetX, evt.offsetY);
+            }
+        });
+    });
     const timer = new Timer();
     // Defining the update require in every frame
     // We only define it. It is called from Timer class
