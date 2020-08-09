b69ea8fe92bf7ed4a02c51fd53ebc86bb066ee76
devanshu <devanshugarg1994@gmail.com>
2020-08-10 00:36:42 +0530

    JSON Restructure and Spriteloader Refactor

--- public/src/Sprite.js
+++ public/src/Sprite.js
@@ -17,16 +17,4 @@ export function loadMarioSprite() {
 });
 }
 
-/* 
-* Loading backgorund spriteSheet  
- */
-export function loadBackgroundSprite() {
-    return loadImage('/img/tiles.png')
-    .then(image => {
-        const sprites = new SpriteSheet(image, 16, 16);
-        sprites.defineTile("ground", 0, 0);
-        sprites.defineTile('sky', 3, 23);
-        return sprites;
 
-});
-}
--- public/src/loader.js
+++ public/src/loader.js
@@ -1,6 +1,8 @@
 import Level from './Level.js';
 import {createBackgroundLayer, createSpriteLayer} from './Layers.js';
-import {loadBackgroundSprite} from './Sprite.js'
+import SpriteSheet from './SpriteSheet.js'
+
+
 /* A utility functiuon to load image */
 export function loadImage(url) {
     return new Promise(resolve => {
@@ -11,31 +13,80 @@ export function loadImage(url) {
         image.src = url;
     });
 }
+
+
 /* 
 * A utility function to add JSON
 */
-export function loadJSON(name) {
-    return fetch(`/levelJSON/${name}.json`)
+export function loadJSON(url) {
+    return fetch(url)
     .then(response => response.json());
 }
-function createTiles(level, backgrounds) {    
-    backgrounds.forEach(backgorund => {
-        backgorund.ranges.forEach(([x1, x2, y1, y2]) => {
-            for(let x = x1; x < x2; x++) {
-                for(let y = y1; y < y2; y++) {
-                    level.tiles.set(x, y, {
-                        name: backgorund.tiles
-                    })
-                }
+/* 
+* A utility function to load SpriteSheet 
+*/
+export function loadSpriteSheet(name) {
+    return loadJSON(`/sprites/${name}.json`)
+        .then(sheetSpec => 
+            Promise.all([
+                sheetSpec,
+                loadImage(sheetSpec.imageURL)
+            ]))
+        .then(([sheetSpec, image]) => {
+            const sprites = new SpriteSheet(image, sheetSpec.tileW, sheetSpec.tileH);
+            sheetSpec.tiles.forEach(tileSpec => {
+                sprites.defineTile(tileSpec.name, tileSpec.index[0], tileSpec.index[1]);
+
+            });
+
+            return sprites;
+        })
+
+
+}
+
+/* 
+* It is function that is used data define in JSON to create tile and map them to a matrix (Need to update how it's further) 
+*/
+function createTiles(level, backgrounds) {
+    function applyRange(background, xStart, xLen, yStart, yLen) {
+        const xEnd = xStart + xLen;
+        const yEnd = yStart + yLen;
+        for (let x = xStart; x < xEnd; ++x) {
+            for (let y = yStart; y < yEnd; ++y) {
+                level.tiles.set(x, y, {
+                    name: background.tiles,
+                });
+            }
+        }
+    }
+    backgrounds.forEach(background => {
+        background.ranges.forEach(range => {
+            if (range.length === 4) {
+                const [xStart, xLen, yStart, yLen] = range;
+                applyRange(background, xStart, xLen, yStart, yLen);
+
+            } else if (range.length === 3) {
+                const [xStart, xLen, yStart] = range;
+                applyRange(background, xStart, xLen, yStart, 1);
+
+            } else if (range.length === 2) {
+                const [xStart, yStart] = range;
+                applyRange(background, xStart, 1, yStart, 1);
             }
         });
     });
 }
+
+/* 
+* A loading function which load everything needed in the level
+*/
 export function loadLevel(name) {
-    return Promise.all([
-        loadJSON('1-1'),
-        loadBackgroundSprite()
-    ])
+    return  loadJSON(`/levelJSON/${name}.json`)
+    .then(levelSpec => Promise.all([
+        levelSpec,
+        loadSpriteSheet(levelSpec.levelName)
+    ]))
     .then(([levelSpec, backgroundSprite]) => { 
         const level = new Level();
         createTiles(level, levelSpec.backgrounds);
